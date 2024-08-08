8e274b145038a2a766c043009b15919dd1df7f83
umesh604@gmail.com <umesh604@gmail.com>
2024-08-08 14:29:57 +0530

    changes commit

---
+++ components/Posts/Post.js
@@ -0,0 +1,153 @@
+import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
+import styled from '@emotion/styled';
+
+const PostContainer = styled.div(() => ({
+  width: '300px',
+  margin: '10px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  overflow: 'hidden',
+}));
+const ListItem = styled.div(() => ({
+ width: "45px",
+ height: "45px",
+ backgroundColor: "gray",
+ borderRadius: "50%",
+ color: "black",
+ fontSize: "14px",
+ textAlign: "center"
+}));
+const User = styled.div(() => ({
+ display: "flex",
+ gap: "10px",
+ margin:"5px 10px"
+}));
+
+
+const CarouselContainer = styled.div(() => ({
+  position: 'relative',
+}));
+
+const Carousel = styled.div(() => ({
+  display: 'flex',
+  overflowX: 'scroll',
+  scrollbarWidth: 'none',
+  msOverflowStyle: 'none',
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+  position: 'relative',
+}));
+
+const CarouselItem = styled.div(() => ({
+  flex: '0 0 auto',
+  scrollSnapAlign: 'start',
+}));
+
+const Image = styled.img(() => ({
+  width: '280px',
+  height: 'auto',
+  maxHeight: '300px',
+  padding: '10px',
+}));
+
+const Content = styled.div(() => ({
+  padding: '10px',
+  '& > h2': {
+    marginBottom: '16px',
+  },
+}));
+
+const Button = styled.button(() => ({
+  position: 'absolute',
+  bottom: 0,
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  border: 'none',
+  color: '#000',
+  fontSize: '20px',
+  cursor: 'pointer',
+  height: '50px',
+}));
+
+const PrevButton = styled(Button)`
+  left: 10px;
+  bottom: 150px;
+`;
+
+const NextButton = styled(Button)`
+  right: 10px;
+  bottom: 150px;
+`;
+
+
+
+
+
+const Post = ({ post }) => {
+  const carouselRef = useRef(null);
+
+  const handleNextClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: 300,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: -300,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return (
+    <PostContainer>
+    <Carousel>
+    <User>
+    <ListItem>
+      <h1>LG</h1>
+    </ListItem>
+    <div>
+    <h3>Leanne Graham</h3>
+    <p>Sincere@april.biz</p>
+    </div>
+    </User>
+    
+    </Carousel>
+    
+      <CarouselContainer>
+        <Carousel ref={carouselRef}>
+          {post.images.map((image, index) => (
+            <CarouselItem key={index}>
+            
+              <Image src={image.url} alt={post.title} />
+            </CarouselItem>
+          ))}
+        </Carousel>
+        <PrevButton onClick={handlePrevClick}>&#10094;</PrevButton>
+        <NextButton onClick={handleNextClick}>&#10095;</NextButton>
+      </CarouselContainer>
+      <Content>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </Content>
+    </PostContainer>
+  );
+};
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    content: PropTypes.any,
+    images: PropTypes.shape({
+      map: PropTypes.func,
+    }),
+    title: PropTypes.any,
+  }),
+};
+
+export default Post;
---
+++ components/UserList/User.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const User = ({users}) => {
+    const [users,setUsers] = useState([])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default User
